4769a63fd4a5a402209320e50e3a5261fe70fb54
Kovalchuk Serhii <free_spirit11@icloud.com>
2024-04-01 15:17:10 -0400

    added Property page

--- utils/requests.js
+++ utils/requests.js
@@ -1,6 +1,14 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
+//Fetch all properties
 async function fetchProperties() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    //Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
+
+    const res = await fetch(`${apiDomain}/properties`);
 
     if (!res.ok) {
       throw new Error('Failed to fetch data');
@@ -8,10 +16,33 @@ async function fetchProperties() {
 
     const data = await res.json();
 
-    return data.properties || [];
+    return data.properties;
   } catch (error) {
     console.log(error);
+    return [];
   }
 }
 
-export { fetchProperties };
+//Fetch single property
+async function fetchProperty(id) {
+  try {
+    //Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await res.json();
+
+    return data.property;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+export { fetchProperties, fetchProperty };
